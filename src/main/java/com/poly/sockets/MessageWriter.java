82d16ba869519cec8831a196dc4de1a41d596794
NastyPill <DJPonnYash.Vinyl@gmail.com>
2021-10-07 17:57:36 +0300

    fix

--- src/main/java/com/poly/sockets/MessageWriter.java
+++ src/main/java/com/poly/sockets/MessageWriter.java
@@ -17,17 +17,12 @@ public class MessageWriter {
 
     private void writeMessage(Message message) {
         String strMessage = message.toTransferString();
-        System.out.println("TRANS SIZE " + strMessage.length());
         try {
             outputStream.write(strMessage.getBytes().length << 24);
             outputStream.write(strMessage.getBytes().length << 16);
             outputStream.write(strMessage.getBytes().length << 8);
             outputStream.write(strMessage.getBytes().length);
             outputStream.write(strMessage.getBytes());
-            printBA(strMessage.getBytes());
-            System.out.println(strMessage);
-            System.out.println(strMessage.getBytes().length);
-            System.out.println(strMessage.length());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -48,13 +43,6 @@ public class MessageWriter {
         if(message.getFileSize() != null && message.getFileSize() > 0
             && message.getFileName() != null && !message.getFileName().isEmpty()) {
             writeFile(messageWithContent.getContent());
-        }
-    }
-
-    public static void printBA(byte[] bytes) {
-        for (int i = 0; i < bytes.length; i++) {
-            System.out.print(bytes[i] + ",");
-            System.out.println();
         }
     }
 }
